025353ae9268ac4d2c0c4f73c0dbff62cbc6c04d
WYHNUS <wangyanhao1993@163.com>
2016-09-30 20:31:40 +0800

    remove unnecessary lines in index.html and index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,6 @@
 import 'core-js/fn/object/assign';
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from './components/Main';
-
 
 // Finally, pass to React Router to display components
 import Router from './routes.js';
