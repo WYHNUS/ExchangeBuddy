d113a8112630256999c00070da1ca0443d5ef643
ZhangHanming <zhang.hanming.official@gmail.com>
2016-10-09 16:56:35 +0800

    add email stmp

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,3 +1,4 @@
+var helper = require('sendgrid').mail;
 var models = require('../models');
 var User = models.User;
 var University = models.University;
@@ -42,6 +43,25 @@ exports.createUser = function(req, res){
         if(!!user && !!homeUniversity && !!exchange){
             user.setUniversity(homeUniversity);
             user.addExchangeEvent(exchange);
+
+            // send verification email
+
+            var from_email = new helper.Email('exchangebuddy@exchangebuddy.com');
+            var to_email = new helper.Email('zhang.hanming.official@gmail.com');
+            var subject = 'Hello World from the SendGrid Node.js Library!';
+            var content = new helper.Content('text/plain', 'Hello, Email!');
+            var mail = new helper.Mail(from_email, subject, to_email, content);
+            var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+            var request = sg.emptyRequest({
+              method: 'POST',
+              path: '/v3/mail/send',
+              body: mail.toJSON(),
+            });
+            sg.API(request, function(error, response) {
+              console.log(response.statusCode);
+              console.log(response.body);
+              console.log(response.headers);
+            });
             res.status(200).send({
                 success: true
             });
