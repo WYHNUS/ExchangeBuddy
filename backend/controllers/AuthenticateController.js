cd0c40637a4981f3f23e968677527dd8830dfffd
WYHNUS <wangyanhao1993@163.com>
2016-10-09 15:42:26 +0800

    update backend authentication logic

--- backend/controllers/AuthenticateController.js
+++ backend/controllers/AuthenticateController.js
@@ -22,114 +22,18 @@ exports.authenticate = function (req, res) {
                 });
         }
 
-        // If user exists, login.
-        // Else, create a login as new user.
-        User.findOrCreate({
+        // Check if use has their emailAccount verified
+        User.findOne({
             where: {
-                fbUserId: response.id,
                 email: response.email
-            },
-            defaults: {
-                name: response.name,
-                gender: req.body.gender,
-                UniversityId: req.body.homeUniversity.id,
             }
-        }).spread(function (user, created) {
-            if (created) {
-                var payload = req.body;
-                /*
-                    Add default three type of groups have unique name:
-                    0: exchange university name + " exchange students -- Year " + year + " " + semester
-                    1: home university name + " going abroad -- Year  " + year + " " + semester
-                    2: home university name + " students in " + exchange university name
-                */
-                /*
-                    Note: it MIGHT be better to retrieve uni name based on id for security reasons
-                */
-                var defaultGroups = [
-                    {
-                        id: 0,
-                        name: payload.exchangeUniversity.name + " exchange students -- Year " + payload.exchangeYear + " " + payload.exchangeSem
-                    },
-                    {
-                        id: 1,
-                        name: payload.homeUniversity.name + " going abroad -- Year  " + payload.exchangeYear + " " + payload.exchangeSem
-                    },
-                    {
-                        id: 2,
-                        name: payload.homeUniversity.name + " students in " + payload.exchangeUniversity.name
-                    }
-                ];
-
-                // For new users, add exchange event
-                Exchange.create({
-                    year: payload.exchangeYear,
-                    term: payload.exchangeSem,
-                    UniversityId: payload.exchangeUniversity.id,
-                    UserId: user.id
-                }).then(function(exchange) {
-                    return Group.findOrCreate({
-                        where: {
-                            name: defaultGroups[0].name,
-                            groupType: defaultGroups[0].id,
-                        }
-                    })
-                }).spread(function(group) {
-                    group.addUser(user);
-                    return Group.findOrCreate({
-                        where: {
-                            name: defaultGroups[1].name,
-                            groupType: defaultGroups[1].id,
-                        }
-                    })
-                }).spread(function(group) {
-                    group.addUser(user);
-                    return Group.findOrCreate({
-                        where: {
-                            name: defaultGroups[2].name,
-                            groupType: defaultGroups[2].id,
-                        }
-                    })
-                }).spread(function(group) {
-                    group.addUser(user);
-                    res.json(user.generateJwt());
-                }).catch(function(err) {
-                    res.status(500).json({
-                        message: err.message
-                    });
-                });
-
-                /*
-                    Don't understand why promise chain doesn't work on method findOrCreate, have to use method above
-                */
-                // Exchange.create({
-                //     year: payload.exchangeYear,
-                //     term: payload.exchangeSem,
-                //     UniversityId: payload.exchangeUniversity.id,
-                //     UserId: user.id
-                // }).then (function(exchange) {
-                //     var defaultGroupArray = defaultGroups.map(group => {
-                //         return Group.findOrCreate({
-                //             where: {
-                //                 name: group.name,
-                //                 groupType: group.id,
-                //             }
-                //         });
-                //     });
-                //     Promise.all(defaultGroupArray).spread((responses) => {
-                //         // console.log(Group.Instance.prototype);
-                //         responses.map(group => {
-                //             group.addUser(user);
-                //         });
-                //         res.json(user.generateJwt());
-                //     })
-                // }).catch(function(err) {
-                //     res.status(500).json({
-                //         message: err.message
-                //     });
-                // });
-            } else {
+        }).then(function (user) {
+            if (user.isEmailVerified) {
                 res.json(user.generateJwt());
+            } else {
+                return res.status(401).json({
+                    message: 'Email account not verified.'
+                });
             }
         }).catch(function(err) {
             return res.status(500).json({
