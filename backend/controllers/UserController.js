cd920f0f9e997200871afcc99088231e8e8e7d85
WYHNUS <wangyanhao1993@163.com>
2016-10-12 01:51:22 +0800

    revert back-end createUser function back, add check if user exist before creating

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -4,6 +4,7 @@ var models = require('../models');
 var User = models.User;
 var University = models.University;
 var Exchange = models.Exchange;
+var Group = models.Group;
 var MailCtrl = require('./MailController');
 var uid = require('rand-token').uid;
 
@@ -24,38 +25,134 @@ exports.getUser = function(req, res) {
 exports.createUser = function(req, res){
     var token = uid(32);
     models.sequelize.Promise.all([
-        User.create({
-            email: req.body.email,
-            name: req.body.name,
-            gender: req.body.gender,
-            fbUserId: req.body.facebookToken,
-            isEmailVerified: 0,
-            verificationToken: token
-        }),
         University.findOne({
             where: {
                 id: req.body.homeUniversity.id
             }
         }),
-        Exchange.findOne({
+        University.findOne({
             where: {
-                year: req.body.exchangeYear,
-                term: req.body.exchangeSem,
-                UniversityId: req.body.exchangeUniversity.id
+                id: req.body.exchangeUniversity.id
+            }
+        }),
+        User.findOne({
+            where: {
+                email: req.body.email
             }
         })
-    ]).spread(function(user, homeUniversity, exchange){
-        if(!!user && !!homeUniversity && !!exchange){
-            user.setUniversity(homeUniversity);
-            user.addExchangeEvent(exchange);
+    ]).spread(function(homeUniversity, exchangeUniversity, existingUser) {
+        if (!existingUser) {
+            return res.status(500).json({
+                    message: "Email account already registered!"
+                });
+        }
+        if (!!homeUniversity && !!exchangeUniversity) {
+            models.sequelize.Promise.all([
+                User.create({
+                    email: req.body.email,
+                    name: req.body.name,
+                    gender: req.body.gender,
+                    fbUserId: req.body.facebookToken,
+                    isEmailVerified: 0, // default to false
+                    UniversityId: req.body.homeUniversity.id
+                    verificationToken: token
+                }),
+                Exchange.create({
+                    year: req.body.exchangeYear,
+                    term: req.body.exchangeSem,
+                    UniversityId: req.body.exchangeUniversity.id
+                })
+            ]).spread(function(user, exchange) {
+                user.addExchangeEvent(exchange);
+                /*
+                    Add default three type of groups have unique name:
+                    0: exchange university name + " exchange students -- Year " + year + " " + semester
+                    1: home university name + " going abroad -- Year  " + year + " " + semester
+                    2: home university name + " students in " + exchange university name
+                */
+                var defaultGroups = [
+                    {
+                        id: 0,
+                        name: exchangeUniversity.name + " exchange students -- Year " + exchange.year + " " + exchange.term
+                    },
+                    {
+                        id: 1,
+                        name: homeUniversity.name + " going abroad -- Year  " + exchange.year + " " + exchange.term
+                    },
+                    {
+                        id: 2,
+                        name: homeUniversity.name + " students in " + exchangeUniversity.name
+                    }
+                ];
 
 
-            MailCtrl.sendVerificationEmail(user);
+                /*
+                    You are about to see a very bad way of writing code... 
+                    Worse than callback hell...
+                    But the code below doesn't work -- group is not a Group instance...
+                    Will improve after a better strategy is discovered...
 
+                    >>>>>>>>>> Ideal solution (not working) <<<<<<<<<<<
+                    
+                    var defaultGroupArray = defaultGroups.map(group => {
+                        return Group.findOrCreate({
+                            where: {
+                                name: group.name,
+                                groupType: group.id,
+                            }
+                        });
+                    });
+                    Promise.all(defaultGroupArray).spread((responses) => {
+                        // console.log(Group.Instance.prototype);
+                        responses.map(group => {
+                            group.addUser(user);
+                        });
+                        res.json(user.generateJwt());
+                    })
+                */
 
-            res.status(200).send({
-                success: true
+                Group.findOrCreate({
+                    where: {
+                        name: defaultGroups[0].name,
+                        groupType: defaultGroups[0].id,
+                    }
+                }).spread(function(group) {
+                    group.addUser(user);
+                    return Group.findOrCreate({
+                        where: {
+                            name: defaultGroups[1].name,
+                            groupType: defaultGroups[1].id,
+                        }
+                    })
+                }).spread(function(group) {
+                    group.addUser(user);
+                    return Group.findOrCreate({
+                        where: {
+                            name: defaultGroups[2].name,
+                            groupType: defaultGroups[2].id,
+                        }
+                    })
+                }).spread(function(group) {
+                    group.addUser(user);
+
+                    // should sent a confirmation email here
+                    MailCtrl.sendVerificationEmail(user);
+
+                    res.status(200)
+                        .json({
+                            success: true,
+                            message: 'Callback Hell reaches to the end.'
+                        });
+
+                });
+            }).catch(function(err){
+                resError(res, err);
             });
+        } else {
+            res.status(400)
+                .json({
+                    message: 'Invalid University Id.'
+                });
         }
     }).catch(function(err){
         resError(res, err);
