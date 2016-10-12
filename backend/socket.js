85ba533d99d019f96f144ea9f55351afa0e60b4d
ZhangHanming <zhang.hanming.official@gmail.com>
2016-10-12 18:02:58 +0800

    store chat msg to db

--- backend/socket.js
+++ backend/socket.js
@@ -1,40 +1,44 @@
 var models = require('./models');
+var ChatCtrl = require('./controller/ChatController');
 var Group = models.Group;
 
 module.exports = function(io){
-    var groupsOfUsers = {}
+    var groupsOfUsers = {};
+    var allGroups = [];
     Group.findAll().then(function(groups){
         for(var group of groups){
             groupsOfUsers[group.name] = [];
+            allGroups.push(group);
         }
     })
 
     io.sockets.on('connection', function(socket){
         console.log('connected');
         socket.on('adduser', function(data){
-            groupsOfUsers[data.group].push(data.user);
+            groupsOfUsers[data.group.name].push(data.user.id);
             socket.user = data.user;
             socket.room = data.group;
-            socket.join(socket.room);
+            socket.join(socket.room.name);
         });
 
         socket.on('sendchat', function(msg){
-            io.sockets.in(socket.room).emit('updatechat', socket.user, msg);
+            ChatCtrl.addChatMessage(socket.user, msg, socket.room);
+            io.sockets.in(socket.room.name).emit('updatechat', socket.user.name, msg);
         });
 
         socket.on('switchroom', function(newroom){
-            socket.leave(socket.room);
-            groupsOfUsers[socket.room].splice(groupsOfUsers[socket.room].indexOf(socket.user), 1);
+            socket.leave(socket.room.name);
+            groupsOfUsers[socket.room.name].splice(groupsOfUsers[socket.room.name].indexOf(socket.user.id), 1);
 
-            socket.join(newroom);
+            socket.join(newroom.name);
             socket.room = newroom;
-            groupsOfUsers[socket.room].push(socket.user);
+            groupsOfUsers[socket.room.name].push(socket.user.id);
 
         });
 
         socket.on('disconnect', function(){
-            socket.leave(socket.room);
-            groupsOfUsers[socket.room].splice(groupsOfUsers[socket.room].indexOf(socket.user), 1);
+            socket.leave(socket.room.name);
+            groupsOfUsers[socket.room.name].splice(groupsOfUsers[socket.room.name].indexOf(socket.user.id), 1);
         })
     });
 }
