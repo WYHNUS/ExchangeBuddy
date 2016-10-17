b44bb42dd42973dd56e11b5a65fbfdcfb69c0a86
kaiyisg <kaiyisg@yahoo.com.sg>
2016-10-18 03:51:51 +0800

    integrating my group endpoint

--- frontend/src/components/HomeComponent/Search/GroupList/GroupList.js
+++ frontend/src/components/HomeComponent/Search/GroupList/GroupList.js
@@ -2,7 +2,6 @@ import React, {PropTypes} from 'react';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 import Loading from '../../../Loading';
 import * as UniversityHelper from '../../../../util/university';
-import IconButton from 'material-ui/IconButton';
 import * as Colors from 'material-ui/styles/colors';
 import { List, ListItem } from 'material-ui/List';
 import Avatar from 'material-ui/Avatar';
@@ -14,64 +13,66 @@ class GroupItem extends React.Component {
   render(){
     const {group, heading, toggleHomeTab, 
       toggleSelectedHomeGroup, toggleHomeSearchDrawerVisibility,
-    homeGroupDetails} = this.props;
+      homeGroupDetails} = this.props;
 
-    const goToGroup = () => { 
-      browserHistory.push(`/home/${group.id}`); 
-      toggleSelectedHomeGroup(group.groupType);
-      toggleHomeTab('events');
-      toggleHomeSearchDrawerVisibility(false); 
-    };
-    return (
-      <div className="group-body">
-      <h5 className="group-heading">{heading}</h5>  
-      <ListItem
-      className={group.groupType===homeGroupDetails.groupType?'selected-group':null}
-      primaryText={group.name}
-      leftAvatar={<Avatar src={ imageUrl } size={ 40 } style={{ objectFit: 'contain', backgroundColor: '#fff'}}/>}
-      onTouchTap={goToGroup}
-      />
-      </div>
-      );
+      const goToGroup = () => { 
+        browserHistory.push(`/home/${group.id}`); 
+        toggleSelectedHomeGroup(group.groupType);
+        toggleHomeTab('events');
+        toggleHomeSearchDrawerVisibility(false); 
+      };
+      return (
+        <div className="group-body">
+        <h5 className="group-heading">{heading}</h5>  
+        <ListItem
+        className={group.groupType===homeGroupDetails.groupType?'selected-group':null}
+        primaryText={group.name}
+        leftAvatar={<Avatar src={ imageUrl } size={ 40 } style={{ objectFit: 'contain', backgroundColor: '#fff'}}/>}
+        onTouchTap={goToGroup}
+        />
+        </div>
+        );
+    }
   }
-}
 
-class Group extends React.Component {
-  render(){
-    const { groupType } = this.props.group;
-    const bSize = 60;
-    const cSize = 60;
-    var uni = "";
-    return (
+  class Group extends React.Component {
+    render(){
+      const { groupType } = this.props.group;
+      const bSize = 60;
+      const cSize = 60; 
+      var uni = "";
+      return (
+        <div>
+        <div className="group-row">
+        {
+          groupType === 0 ? <GroupItem {...this.props} heading={"Your Exchange University Group"} />
+          : groupType === 1 ? <GroupItem {...this.props} heading={"Your Home University Group"}/>
+          : groupType === 2 ? <GroupItem {...this.props} heading={"Your Home University Alumni Support Group"}/>
+          : groupType === 3 ? <GroupItem {...this.props} heading={"Your Special Group"}/>
+          : null
+        }
+        </div>
+        </div>
+        );
+    }
+  }
+
+
+  export default class GroupList extends React.Component {
+
+   render() {
+    const {groups, user, toggleHomeSearchDrawerVisibility, toggleSelectedHomeGroup} = this.props;
+    return(
       <div>
-      <div className="group-row">
-      {
-        groupType === 0 ? <GroupItem {...this.props} heading={"Your Exchange University Group"} />
-        : groupType === 1 ? <GroupItem {...this.props} heading={"Your Home University Group"}/>
-        : groupType === 2 ? <GroupItem {...this.props} heading={"Your Home University Alumni Support Group"}/>
-        : groupType === 3 ? <GroupItem {...this.props} heading={"Your Special Group"}/>
-        : null
-      }
-      </div>
-      </div>
-      );
-  }
-}
-
-
-export default class GroupList extends React.Component {
-
- render() {
-  const {groups, user, toggleHomeSearchDrawerVisibility, toggleSelectedHomeGroup} = this.props;
-  return(
-   <List className="groups-container">
-   { groups.length > 0 && groups.map((group, idx) => 
-    <Group group={group} {...this.props} key={idx}/>
+      <List className="groups-container">
+      { groups.length > 0 && groups.map((group, idx) => 
+        <Group group={group} {...this.props} key={idx}/>
         /*<Group group={ group } currentUser={ user } key={ idx } 
         handleClose={toggleHomeSearchDrawerVisibility} toggleSelectedHomeGroup={toggleSelectedHomeGroup} />*/) }
-   </List>
-   );
-}
+      </List>
+      </div>
+      );
+  }
 }
 
 GroupList.PropTypes={
