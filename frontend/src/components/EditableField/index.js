2a8101f71b28eb9feea019eb5cbde8fed0cb93dd
WYHNUS <wangyanhao1993@163.com>
2016-10-19 15:02:01 +0800

    add font size and family in journal

--- frontend/src/components/EditableField/index.js
+++ frontend/src/components/EditableField/index.js
@@ -6,8 +6,18 @@ export const EditableField = ({ name, content, ...rest }) => {
     name={name}
     content= {content}
     config={{
-      plugins: 'link image code media imagetools fullscreen autoresize',
-      toolbar: 'undo redo | bold italic | alignleft aligncenter alignright | code'
+    	menu: {
+			file: {title: 'File', items: 'newdocument'},
+			edit: {title: 'Edit', items: 'undo redo | cut copy paste pastetext | selectall'},
+			insert: {title: 'Insert', items: 'link media | template hr'},
+			// view: {title: 'View', items: 'visualaid'},
+			format: {title: 'Format', items: 'bold italic underline strikethrough superscript subscript | removeformat'},
+			table: {title: 'Table', items: 'inserttable tableprops deletetable | cell row column'},
+			tools: {title: 'Tools', items: 'spellchecker code'}
+		},
+		plugins: 'link image media imagetools autoresize',
+		toolbar: 'fontselect fontsizeselect | undo redo | bold italic | alignleft aligncenter alignright | link image',
+		fontsize_formats: '8pt 10pt 12pt 14pt 18pt 24pt 36pt',
     }}
     {...rest} />
 };
