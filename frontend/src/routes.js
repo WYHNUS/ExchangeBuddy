d92747d0747f444804ce8c509b671f4f35b6e460
WYHNUS <wangyanhao1993@163.com>
2016-10-22 13:47:51 +0800

    setup GA

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -34,6 +34,15 @@ const store = configureStore();
 // Create an enhanced history that syncs navigation events with the store
 const history = syncHistoryWithStore(browserHistory, store);
 
+// add GA
+var ReactGA = require('react-ga');
+ReactGA.initialize(process.env.GA_ID);
+ 
+function logPageView() {
+  ReactGA.set({ page: window.location.pathname });
+  ReactGA.pageview(window.location.pathname);
+}
+
 export const getRoutes = (store) =>{
 
   const authRequired = (nextState, replace) => {
@@ -92,7 +101,7 @@ export const getRoutes = (store) =>{
 
 export default (
       <Provider store={ store }>
-        <Router history={ history }>
+        <Router history={ history } onUpdate={ logPageView } >
           { getRoutes(store) }
         </Router>
       </Provider>
