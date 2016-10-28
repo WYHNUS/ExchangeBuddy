f462ed485ad1934ca67dcdf23a8f78208cd43a0a
Zhang Hanming <zhanghanming@u.nus.edu>
2016-10-29 03:31:55 +0800

    Revert "add ssl cert"
    This reverts commit 1c0358b6612cd3c6ba1c72c18bfd9d49de19a42a.

--- frontend/bin/server.js
+++ frontend/bin/server.js
@@ -3,16 +3,8 @@ require('dotenv').config();
 
 const config = require('../config')
 const server = require('../server/main')
-const https = require('https');
 const debug = require('debug')('app:bin:server')
-const fs = require('fs')
 const port = config.server_port
 
-const options = {
-    key: fs.readFileSync('bin/key.pem'),
-    cert: fs.readFileSync('bin/cert.pem')
-}
-
-https.createServer(options, (req, res) => {
-}).listen(port);
+server.listen(port)
 debug(`Server is now running at http://localhost:${port}.`)
