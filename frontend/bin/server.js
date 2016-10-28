1c0358b6612cd3c6ba1c72c18bfd9d49de19a42a
Zhang Hanming <zhanghanming@u.nus.edu>
2016-10-29 01:33:12 +0800

    add ssl cert

--- frontend/bin/server.js
+++ frontend/bin/server.js
@@ -3,8 +3,16 @@ require('dotenv').config();
 
 const config = require('../config')
 const server = require('../server/main')
+const https = require('https');
 const debug = require('debug')('app:bin:server')
+const fs = require('fs')
 const port = config.server_port
 
-server.listen(port)
+const options = {
+    key: fs.readFileSync('bin/key.pem'),
+    cert: fs.readFileSync('bin/cert.pem')
+}
+
+https.createServer(options, (req, res) => {
+}).listen(port);
 debug(`Server is now running at http://localhost:${port}.`)
