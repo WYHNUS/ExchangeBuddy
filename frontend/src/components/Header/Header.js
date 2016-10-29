1077dd49ff049c8a7388d357c6f3eb732abee249
WYHNUS <wangyanhao1993@163.com>
2016-10-29 21:12:36 +0800

    fix home page header display bug

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -27,7 +27,7 @@ const gotourl = (groupId, tab) => () => {
 
 const tabToIdx = tab => {
   //console.log(tab,'tab');
-  switch(tab) {
+  switch (tab) {
     case 'events':
       return 0;
     case 'chat':
@@ -44,9 +44,9 @@ const pathToIdx = () =>{
   var pathArray = window.location.pathname.split("/");
   console.log(pathArray);
   var path;
-  if(pathArray.length>3){
+  if (pathArray.length > 3) {
     path = pathArray[3];
-  }else{
+  } else {
     path='events';
   }
   //console.log(path);
@@ -166,19 +166,19 @@ export default class Header extends React.Component {
 
             {
               !!homeGroupDetails.name ? 
-                (parseInt(homeGroupDetails.groupType) !== 2) ?
-                (
-                  <div className='col-xs-12 col-md-8 col-lg-8' id="header-title">
-                    <h2 id="uni-name">{ homeGroupDetails.name }</h2>
-                    <p id="uni-description">{ /*`${ group.term } ${ group.year } - ${ group.users.length } ${ pluralizer(group.users.length, 'Member', 'Members') }`*/ }</p>
-                  </div>
-                ):
-                (
-                  <div className='col-xs-12 col-md-8 col-lg-8' id="header-title">
-                    <h2 id="uni-name">{ getName(homeGroupDetails.name) }</h2>
-                    <p id="uni-description">{ `${getTerm(homeGroupDetails.name)} ${getYear(homeGroupDetails.name)}`/*`${ group.term } ${ group.year } - ${ group.users.length } ${ pluralizer(group.users.length, 'Member', 'Members') }`*/ }</p>
-                  </div>
-                )
+                (parseInt(homeGroupDetails.groupType) == 2) ?
+                  (
+                    <div className='col-xs-12 col-md-8 col-lg-8' id="header-title">
+                      <h2 id="uni-name">{ homeGroupDetails.name }</h2>
+                      <p id="uni-description">{ /*`${ group.term } ${ group.year } - ${ group.users.length } ${ pluralizer(group.users.length, 'Member', 'Members') }`*/ }</p>
+                    </div>
+                  )
+                : (
+                    <div className='col-xs-12 col-md-8 col-lg-8' id="header-title">
+                      <h2 id="uni-name">{ getName(homeGroupDetails.name) }</h2>
+                      <p id="uni-description">{ `${getTerm(homeGroupDetails.name)} ${getYear(homeGroupDetails.name)}`/*`${ group.term } ${ group.year } - ${ group.users.length } ${ pluralizer(group.users.length, 'Member', 'Members') }`*/ }</p>
+                    </div>
+                  )
               : null
             }
 
