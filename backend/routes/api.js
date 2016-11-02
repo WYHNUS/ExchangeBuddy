7171b2f2f6db9260dd0c89a4f1c954e87604d272
Zhang Hanming <zhanghanming@u.nus.edu>
2016-11-02 20:12:27 +0800

    add joinGroup api

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -83,6 +83,7 @@ request:
     userId: 1
 }
 */
+router.post('/joinGroup', verifyToken, GroupCtrl.joinGroup);
 router.post('/group', verifyToken, GroupCtrl.getGroupIndex);
 router.get('/group/:id', verifyToken, GroupCtrl.getGroup);
 router.get('/getGroups', verifyToken, GroupCtrl.getGroups);
