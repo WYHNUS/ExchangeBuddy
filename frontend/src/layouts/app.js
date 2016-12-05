c55fb640a9d70240a93d029d31641e4a51a733f3
Irvin Lim <irvinlimweiquan@gmail.com>
2016-12-05 14:02:41 +0800

    Fix code style in layouts/app

--- frontend/src/layouts/app.js
+++ frontend/src/layouts/app.js
@@ -20,7 +20,7 @@ import Joyride from 'react-joyride';
 
 class App extends React.Component{
 
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.state = {
@@ -30,86 +30,72 @@ class App extends React.Component{
   }
 
   componentDidUpdate(prevProps, prevState) {
-
-    if(this.props.homeJoyride.start){
+    if (this.props.homeJoyride.start) {
       setTimeout(() => {
-            this.setState({
-              ready: true
-            });
-        }, 1000);
-        if (!prevState.ready && this.state.ready) {
-          console.log('here joy ride starts');
+          this.setState({ ready: true });
+      }, 1000);
+      
+      if (!prevState.ready && this.state.ready) {
+        console.log('here joy ride starts');
         this.joyride.start(true);
       }
     }
   }
 
-  render(){
+  render() {
+    const { homeJoyride } = this.props;
 
-    const{homeJoyride} = this.props;
+    return (
+      <MuiThemeProvider muiTheme={ MuiTheme }>
+        <div>
+          <Joyride 
+            ref={ c => (this.joyride = c) } 
+            steps={ this.state.steps } 
+            debug={ false }
+            type="continuous"
+            showSkipButton
+            showStepsProgress
+            showOverlay />
 
-    return(
+          <TopBar 
+            onTouchTap={ () => this.props.toggleHomeSearchDrawerVisibility(false) }
+            addSteps={ this.addSteps } />
 
-      <MuiThemeProvider muiTheme={MuiTheme}>
+          <div id="root-container">
+            <Helmet
+              defaultTitle="ExchangeBuddy - Find your exchange buddies!"
+              meta={
+                [
+                  {'name': 'description', 'content': 'ExchangeBuddy - Find your exchange buddies!'},
+                  {'name': 'viewport', 'content': 'initial-scale=1, minimal-ui, maximum-scale=1, minimum-scale=1'},
+                  {'property': 'og:type', 'content': 'exchangebuddy:exchange_group'},
+                  {'property': 'og:url', 'content': 'http://app.exchangebuddy.com/'},
+                  {'property': 'og:description', 'content': 'Find your exchange buddies!'},
+                  {'property': 'og:title', 'content': 'ExchangeBuddy'}
+                ]
+              }
+              link={
+                [
+                  {'rel': 'canonical', 'href': 'http://app.exchangebuddy.com'},
+                  {'rel': 'shortcut icon', 'href': 'favicon.png?v1', 'type': 'image/png', 'sizes': '16x16 32x32 64x64'},
+                  {'rel': 'apple-touch-icon', 'sizes': '120x120', 'href': 'apple-touch-icon-precomposed.png'}
+                ]
+              } />
+            <div id="main" className={`page-${ makeRouteSlug(this.props.routes) }`}>
+              { this.props.children }
+            </div>
 
-      <div>
+            <Search addSteps={this.addSteps}/>
+          </div>
 
-      <Joyride 
-        ref={c => (this.joyride = c)} 
-        steps={this.state.steps} 
-        debug={false}
-        type="continuous"
-        showSkipButton={true}
-        showStepsProgress={true}
-        showOverlay={true} />
-
-      <TopBar 
-        onTouchTap={()=>this.props.toggleHomeSearchDrawerVisibility(false)}
-        addSteps={this.addSteps} />
-
-      <div id="root-container">
-
-      <Helmet
-        defaultTitle="ExchangeBuddy - Find your exchange buddies!"
-        meta={
-          [
-            {'name': 'description', 'content': 'ExchangeBuddy - Find your exchange buddies!'},
-            {'name': 'viewport', 'content': 'initial-scale=1, minimal-ui, maximum-scale=1, minimum-scale=1'},
-            {'property': 'og:type', 'content': 'exchangebuddy:exchange_group'},
-            {'property': 'og:url', 'content': 'http://app.exchangebuddy.com/'},
-            {'property': 'og:description', 'content': 'Find your exchange buddies!'},
-            {'property': 'og:title', 'content': 'ExchangeBuddy'}
-          ]
-        }
-        link={
-          [
-            {'rel': 'canonical', 'href': 'http://app.exchangebuddy.com'},
-            {'rel': 'shortcut icon', 'href': 'favicon.png?v1', 'type': 'image/png', 'sizes': '16x16 32x32 64x64'},
-            {'rel': 'apple-touch-icon', 'sizes': '120x120', 'href': 'apple-touch-icon-precomposed.png'}
-          ]
-        } />
-
-
-      <div id="main" className={`page-${ makeRouteSlug(this.props.routes) }`}>
-      { this.props.children }
-      </div>
-
-      <Search addSteps={this.addSteps}/>
-
-
-      </div>
-
-      <MessageSnackbar/>
-
-      <BottomBar addSteps={this.addSteps}/>
-
-      </div>
-
+          <MessageSnackbar/>
+          <BottomBar addSteps={this.addSteps}/>
+        </div>
       </MuiThemeProvider>
       );
   }
 
-  addSteps=(steps)=>{
+  addSteps = (steps) => {
     const joyride = this.joyride;
     let newSteps = steps;
 
@@ -132,7 +118,7 @@ const mapStateToProps = (state)=>{
   return {
     homeJoyride: state.home.homeJoyride
   };
-}
+};
 
 const mapDispatchToProps = (dispatch) => {
   return {
