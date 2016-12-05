e98c6b34984f8531364a9de622e716046ba4171f
Irvin Lim <irvinlimweiquan@gmail.com>
2016-12-05 15:24:36 +0800

    Fix warnings

--- frontend/src/layouts/app.js
+++ frontend/src/layouts/app.js
@@ -1,4 +1,4 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
 import MuiTheme from './mui-theme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Helmet from 'react-helmet';
