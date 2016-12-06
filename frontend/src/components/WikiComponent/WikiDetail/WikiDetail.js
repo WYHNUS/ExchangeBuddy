9459c3baea6f09fc0058922a66c8cbedc7e0b80e
Irvin Lim <irvinlimweiquan@gmail.com>
2016-12-06 17:21:28 +0800

    Convert to dumb component

--- frontend/src/components/WikiComponent/WikiDetail/WikiDetail.js
+++ frontend/src/components/WikiComponent/WikiDetail/WikiDetail.js
@@ -1,34 +1,28 @@
 import React, {PropTypes} from 'react';
 
-import AvatarRow from 'components/AvatarRow';
-import WikiContentTable from '../WikiContentTable'
 import WikiSection from '../WikiSection';
 import WikiAddSectionButton from '../WikiAddSectionButton';
 
-export default class WikiDetail extends React.Component {
-  render() {
-    const { wiki, sections } = this.props;
+const WikiDetail = ({ wiki, sections }) => (
+  <div className="container">
+    <div className="row">
+      <div className="col-xs wiki-detail-wrapper">
+        <h1>{ wiki.title }</h1>
+        <h4>All information is contributed by people like you!</h4>
+        
+        { sections.length > 0 && sections.map((section, idx) => 
+          <WikiSection wiki={ wiki } section={ section } key={ idx } />
+        ) }
 
-    return (
-      <div className="container">
-        <div className="row">
-          <div className="col-xs wiki-detail-wrapper">
-            <h1>{ wiki.title }</h1>
-            <h4>All information is contributed by people like you!</h4>
-            
-            { sections.length > 0 && sections.map((section, idx) => 
-              <WikiSection wiki={ wiki } section={ section } key={ idx } />
-            ) }
-
-            <WikiAddSectionButton wikiTitle={ wiki.title } />
-          </div>
-        </div>
+        <WikiAddSectionButton wikiTitle={ wiki.title } />
       </div>
-    );
-  }
-}
+    </div>
+  </div>
+);
 
 WikiDetail.propTypes = {
   wiki: PropTypes.object.isRequired,
   sections: PropTypes.array.isRequired
 };
+
+export default WikiDetail;
