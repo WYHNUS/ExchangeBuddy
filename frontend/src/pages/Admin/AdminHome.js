4dacf1f4b70653eedbdc4c8bf990c4e931525ec3
Irvin Lim <irvinlimweiquan@gmail.com>
2016-12-12 14:34:47 +0800

    Add AdminDashboardButton

--- frontend/src/pages/Admin/AdminHome.js
+++ frontend/src/pages/Admin/AdminHome.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
+import AdminDashboardButton from 'components/Admin/AdminDashboardButton';
+
 const AdminHome = () => (
   <div className="admin-home container">
     <div className="row">
       <div className="col-xs">
         <h1>Admin Dashboard</h1>
+
+        <div className="admin-dashboard-container">
+          <AdminDashboardButton icon="school" label="Manage Universities" description="Create and update universtities here." link="/admin/universities" />
+        </div>
       </div>
     </div>
   </div>
