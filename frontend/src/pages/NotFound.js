604d253e7ba3f7927c420fccad8b1c57d6485d49
Irvin Lim <irvinlimweiquan@gmail.com>
2016-12-12 14:01:17 +0800

    Design 404 page

--- frontend/src/pages/NotFound.js
+++ frontend/src/pages/NotFound.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Link from 'components/Link';
+import Icon from 'components/Icon';
+
+import * as Colors from 'material-ui/styles/colors';
 
 export default () => (
   <div className="row center-xs" style={{ margin: 40 }}>
-    <div className="col-xs-8">
-      <h2>404 - Sorry, page not found</h2>
-      <Link id="home-button" to="/">Back to home</Link>
+    <div className="col-xs-12 col-sm-8">
+      <Icon name="bug_report" size={120} color={ Colors.grey500 } style={{ marginTop: 30 }} />
+      <h1 style={{ fontSize: 48, fontWeight: 100, margin: '20px 0' }}>404</h1>
+      <p>The page you are looking for cannot be found.</p>
+      <p style={{ margin: '20px 0' }}><Link id="home-button" to="/">Back to home</Link></p>
     </div>
   </div>
 );
