1161e8f478f36d79897586c83ce2266c696ea460
ZhangHanming <zhang.hanming.official@gmail.com>
2016-12-21 21:18:10 +0800

    add feedpostcommentreply controller skeleton

---
+++ backend/controllers/FeedPostReplyController.js
@@ -0,0 +1,13 @@
+var models = require('../models');
+
+exports.createReply = function(req, res){
+
+}
+
+exports.deleteReply = function(req, res){
+
+}
+
+exports.updateReply = function(req, res){
+    
+}
