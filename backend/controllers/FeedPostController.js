b8f529ba795b76854c5985fefc2eb4a35d20612a
ZhangHanming <zhang.hanming.official@gmail.com>
2016-12-21 21:14:41 +0800

    add feedpost controller skeleton

---
+++ backend/controllers/FeedPostController.js
@@ -0,0 +1,25 @@
+var models = require("../models");
+
+exports.createFeedPost = function(req, res){
+
+}
+
+exports.getFeedPostByGroup = function(req, res){
+
+}
+
+exports.deleteFeedPost = function(req, res){
+
+}
+
+exports.updateFeedPost = function(req, res){
+
+}
+
+exports.reactToFeedPost = function(req, res){
+
+}
+
+exports.unreactToFeedPost = function(req, res){
+
+}
