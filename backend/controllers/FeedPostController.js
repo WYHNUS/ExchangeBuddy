97e8b3749df54c910fe1ed1e09623bfeceab7d1a
ZhangHanming <zhang.hanming.official@gmail.com>
2016-12-21 21:58:34 +0800

    clean up api routes

--- backend/controllers/FeedPostController.js
+++ backend/controllers/FeedPostController.js
@@ -1,7 +1,23 @@
 var models = require("../models");
 
 exports.createFeedPost = function(req, res){
-
+    if(!!req.body.content && !!req.body.GroupId){
+        models.FeedPost.create({
+            authorId: req.user.id,
+            content: req.body.content,
+            GroupId: req.body.GroupId,
+        }).then(function(feedpost){
+            res.status(200).send({
+                status: 'success',
+                message: 'Feedpost created',
+            })
+        })
+    }else{
+        res.status(400).send({
+            status: 'fail',
+            message: 'missing content or GroupId'
+        })
+    }
 }
 
 exports.getFeedPostByGroup = function(req, res){
--- backend/routes/api.js
+++ backend/routes/api.js
@@ -25,130 +25,73 @@ router.get('/', function(req, res) {
     });
 });
 
+// AUTHENTICATE CONTROLLER
+
 // Authenticate with Facebook access token or email with password
 router.post('/authenticateOrCreateByFB', AuthCtrl.authenticateOrCreateByFB);
 router.post('/authenticateByEmail', AuthCtrl.authenticateByEmail);
-// Verify JSWT
-router.get('/me', verifyToken, function(req, res) {
-    res.send(req.user);
-});
 
-/*
-request:
-{
-    userId: 1,
-    exchangeUniversityId: 1,
-    homeUniversityId: 2,
-    term: fall/spring,
-    year: 2017
-}
-*/
+
+// USER CONTROLLER
 router.patch('/updateUni', UserCtrl.updateUni);
-
 router.get('/user/:id', verifyToken, UserCtrl.getUser);
-
-/*
-request:
-{
-    bio: "abc",
-    website: "a.com",
-    birthday: 1234567 (in milliseconds),
-    name: "haha",
-}
-*/
 router.patch('/updateUser', verifyToken, UserCtrl.updateUser);
 router.put('/createUser', UserCtrl.createUser);
 router.post('/uploadProfile',[verifyToken, upload.single('profilePicture'), UserCtrl.uploadProfile]);
 
+
+// UNIVERSITY CONTROLLER
 router.patch('/updateUniInfo', verifyToken, UniCtrl.updateUniInfo);
 router.post('/updateUniLogo', [verifyToken, upload.single('uniLogo'), UniCtrl.updateUniLogo]);
-
-router.get('/verify/:token', MailCtrl.verifyToken);
-router.get('/resendVerificationMail/:userId', MailCtrl.resend);
-
-// countries and universities data are public (no need to authenticate)
-router.get('/country', CountryCtrl.getAllCountries);
-router.get('/country/:id', CountryCtrl.getCountry);
-
 router.get('/universities', UniCtrl.getAllUniversities);
-/*
-request:
-{
-    name: "National University of Singapore",
-    alpha2Code: "AD"
-}
-*/
 router.put('/university', verifyToken, UniCtrl.createUniversity);
 router.get('/university/:id', UniCtrl.getUniversity);
 router.get('/universitiesByCountry/:alpha2Code', UniCtrl.getAllUniversitiesForCountry);
 
-router.post('/messages', verifyToken, ChatCtrl.getMessages);
 
+// MAIL CONTROLLER
+router.get('/verify/:token', MailCtrl.verifyToken);
+router.get('/resendVerificationMail/:userId', MailCtrl.resend);
+
+// COUNTRY CONTROLLER
+// countries and universities data are public (no need to authenticate)
+router.get('/country', CountryCtrl.getAllCountries);
+router.get('/country/:id', CountryCtrl.getCountry);
+
+// WIKI CONTROLLER
 // use query String to get particular wiki page
 router.get('/wikiRecommend', WikiCtrl.getRecommendation);
 router.get('/wikiCustomizedRecommend', verifyToken, WikiCtrl.getCustomizedRecommendation);
 router.get('/wiki', WikiCtrl.getWiki);  // ?q= &param= [stringified array: {section= &version=}]
 router.put('/wiki', verifyToken, WikiCtrl.createNewWiki);
-/*
-request:
-{
-    wikiTitle: Singapore,
-    versionTitle: History,
-    content: <div><p>Singapore has a loooong history. :)</p></div>
-}
-*/
 router.put('/wiki/section', verifyToken, WikiCtrl.createNewSection);
-/*
-request:
-{
-    wikiTitle: Singapore,
-    sectionIndex: 10
-}
-*/
 router.delete('/wiki/section', verifyToken, WikiCtrl.deleteSection);
 router.get('/wiki/section/allVersions', WikiCtrl.getWikiSectionAllVersions);  // ?q= &sectionIndex=
-/*
-request:
-{
-    wikiTitle: Singapore,
-    sectionIndex: 1,
-    sectionTitle: Food,
-    content: <div><p>Singapore has a variety of nice food. :)</p></div>
-}
-*/
 router.put('/wiki/section/version', verifyToken, WikiCtrl.createNewSectionVersion);
 // router.post('/wiki/section/version/vote', verifyToken, WikiCtrl.vote);
 
+
+// STORY CONTROLLER
 router.get('/allStories', /*verifyToken,*/ StoryCtrl.getAllStories);
 router.post('/story/:id', /*verifyToken,*/ StoryCtrl.getStory);
 router.put('/story', verifyToken, StoryCtrl.createStory);
 
-/*
-get the groups a user currently belongs to
-request:
-{
-    userId: 1
-}
-*/
+
+// GROUP CONTROLLER
 router.post('/joinGroup', verifyToken, GroupCtrl.joinGroup);
 router.post('/leaveGroup', verifyToken, GroupCtrl.leaveGroup);
 router.post('/group', verifyToken, GroupCtrl.getGroupIndex);
 router.get('/group/:id', verifyToken, GroupCtrl.getGroup);
 router.get('/getGroups', GroupCtrl.getGroups);
-
-/*
-Get group members
-request:
-{
-    GroupId: 1
-}
-*/
 router.post('/members', verifyToken, GroupCtrl.getMembers);
 
-
+// MISC
 router.get('/signups', function(req, res){
     models.User.findAll().then(function(users){
         res.send('Number of users: ' + users.length)
     })
 })
+router.get('/me', verifyToken, function(req, res) {
+    res.send(req.user);
+});
 module.exports = router;
