042d7d0bfcd57f3733ed5f8da40d3a1942fc6489
ZhangHanming <zhang.hanming.official@gmail.com>
2016-12-21 21:16:49 +0800

    add feedpostcomment controller skeleton

---
+++ backend/controllers/FeedPostCommentController.js
@@ -0,0 +1,21 @@
+var models = require('../models');
+
+exports.createComment = function(req, res){
+
+}
+
+exports.updateComment = function(req, res){
+
+}
+
+exports.deleteComment = function(req, res){
+
+}
+
+exports.reactToComment = function(req, res){
+
+}
+
+exports.unreactToComment = function(req, res){
+
+}
