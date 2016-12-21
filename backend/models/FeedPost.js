8d5489c97d62bce12d0e1c359b6bdc8600bef794
ZhangHanming <zhang.hanming.official@gmail.com>
2016-12-21 21:10:05 +0800

    associate FeedPost to Group

--- backend/models/FeedPost.js
+++ backend/models/FeedPost.js
@@ -14,6 +14,10 @@ module.exports = function(sequelize, DataType){
                     as: "author"
                 });
 
+                FeedPost.belongsTo(models.Group, {
+                    onDelete: "CASCADE",
+                })
+
                 FeedPost.hasMany(models.FeedPostComment);
                 FeedPost.hasMany(models.FeedPostReaction);
             }
