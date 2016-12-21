1a8d522aaebc555f4931aaf023c714eaa22a840f
Irvin Lim <irvinlimweiquan@gmail.com>
2016-12-21 12:31:41 +0800

    Add profile button in topbar

--- frontend/src/components/TopBar/TopBar.js
+++ frontend/src/components/TopBar/TopBar.js
@@ -6,7 +6,7 @@ import Link from 'components/Link';
 import Icon from 'components/Icon';
 import AppBar from 'material-ui/AppBar';
 
-import { isUserAdmin } from 'util/user';
+import { isUserAdmin, getAvatar } from 'util/user';
 import * as Colors from 'material-ui/styles/colors';
 
 import './TopBar.scss';
@@ -20,19 +20,18 @@ export const menuItems = [
 ];
 
 const TopBar = ({ user }) => (
-  <AppBar 
-    className="topbar" 
-    showMenuIconButton={false} 
-    title={
-      <div className="container">
-        <div className="row">
-          <div className="col col-xs-12 col-sm-6 center-xs start-sm">
-            <Link to="/">
-              <Icon name="fa fa-globe" color={ Colors.grey50 } size={28} /> ExchangeBuddy
-            </Link>
-          </div>
-          <div className="col col-xs-6 end-xs hidden-xs">
-            <div className="menu-items">
+  <div className="topbar">
+    <AppBar 
+      showMenuIconButton={false} 
+      title={
+        <div className="container">
+          <div className="row">
+            <div className="col col-xs-12 col-sm-6 center-xs start-sm">
+              <Link to="/">
+                <Icon name="fa fa-globe" color={ Colors.grey50 } size={28} /> ExchangeBuddy
+              </Link>
+            </div>
+            <div className="col col-xs-6 end-xs middle-xs hidden-xs">
               { menuItems.map((item, idx) => {
                   if (item.admin && !isUserAdmin(user))
                     return null;
@@ -47,12 +46,16 @@ const TopBar = ({ user }) => (
                     </IconButton>
                   );
                 }) }
+
+                <IconButton onClick={ () => browserHistory.push('/profile') } tooltip="Profile" tooltipStyles={{ fontWeight: 400, fontSize: 12 }}>
+                  { getAvatar(user, 24) }
+                </IconButton>
             </div>
           </div>
         </div>
-      </div>
-    }
-    titleStyle={{ fontWeight: 100, fontSize: 28, overflow: 'visible' }} />
+      }
+      titleStyle={{ fontWeight: 100, fontSize: 28, overflow: 'visible' }} />
+    </div>
 );
 
 TopBar.propTypes = {
