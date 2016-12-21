a60f55765902967ab64ecc499b9e8579f95dde2c
Irvin Lim <irvinlimweiquan@gmail.com>
2016-12-21 18:54:42 +0800

    Fix BottomBar

--- frontend/src/components/TopBar/TopBar.js
+++ frontend/src/components/TopBar/TopBar.js
@@ -11,12 +11,13 @@ import * as Colors from 'material-ui/styles/colors';
 
 import './TopBar.scss';
 
-export const menuItems = [
+export const makeMenuItems = (user) => [
   { label: 'Admin Area', icon: 'fa fa-wrench', to: '/admin', admin: true },
   { label: 'Your Exchange Group', icon: 'fa fa-users', to: '/group' },
   { label: 'WikiExchange', icon: 'fa fa-graduation-cap', to: '/wiki' },
   { label: 'ExchangeAnswers', icon: 'fa fa-comments', to: '/qna' },
   { label: 'Stories', icon: 'fa fa-newspaper-o', to: '/stories' },
+  { label: 'Profile', avatar: getAvatar(user, 24), to: '/profile' },
 ];
 
 const TopBar = ({ user }) => (
@@ -34,7 +35,7 @@ const TopBar = ({ user }) => (
               </div>
             </div>
             <div className="col col-xs-6 end-xs middle-xs hidden-xs">
-              { menuItems.map((item, idx) => {
+              { makeMenuItems(user).map((item, idx) => {
                   if (item.admin && !isUserAdmin(user))
                     return null;
 
@@ -44,14 +45,13 @@ const TopBar = ({ user }) => (
                       onClick={ () => browserHistory.push(item.to) } 
                       tooltip={ item.label } 
                       tooltipStyles={{ fontWeight: 400, fontSize: 12 }}>
-                      <Icon name={ item.icon } size={20} color={ Colors.grey50 } />
+                      { item.icon 
+                        ? <Icon name={ item.icon } size={20} color={ Colors.grey50 } /> 
+                        : item.avatar
+                      }
                     </IconButton>
                   );
                 }) }
-
-                <IconButton onClick={ () => browserHistory.push('/profile') } tooltip="Profile" tooltipStyles={{ fontWeight: 400, fontSize: 12 }}>
-                  { getAvatar(user, 24) }
-                </IconButton>
             </div>
           </div>
         </div>
