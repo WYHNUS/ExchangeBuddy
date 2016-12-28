f76e835613932fc56ea54af577afb8f645db6f7d
ZhangHanming <zhang.hanming.official@gmail.com>
2016-12-28 12:02:58 +0800

    add Question Controller skeleton

---
+++ backend/controllers/QuestionController.js
@@ -0,0 +1,25 @@
+var models = require('../models');
+
+exports.createQuestion = function(req, res){
+
+}
+
+exports.updateQuestion = function(req, res){
+
+}
+
+exports.deleteQuestion = function(req, res){
+
+}
+
+exports.getQuestions = function(req, res){
+
+}
+
+exports.voteQuestionUp = function(req, res){
+
+}
+
+exports.voteQuestionDown = function(req, res){
+    
+}
