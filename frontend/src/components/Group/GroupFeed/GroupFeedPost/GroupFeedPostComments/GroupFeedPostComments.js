74ef4a8dda6c08e1d4f57e0046e5a9165549fb8a
Irvin Lim <irvinlimweiquan@gmail.com>
2016-12-29 11:48:23 +0800

    Change style of comments

--- frontend/src/components/Group/GroupFeed/GroupFeedPost/GroupFeedPostComments/GroupFeedPostComments.js
+++ frontend/src/components/Group/GroupFeed/GroupFeedPost/GroupFeedPostComments/GroupFeedPostComments.js
@@ -4,6 +4,8 @@ import GroupFeedPostWriteComment from '../GroupFeedPostWriteComment';
 
 import { groupPropType, userPropType, feedPostCommentPropType, feedPostPropType } from 'util/propTypes';
 
+import './GroupFeedPostComments.scss';
+
 const GroupFeedPostComments = ({ feedComments, group, feedPost, refreshComments }) => (
   <div className="comments-main-container">
     { feedComments.map((comment, idx) => (
