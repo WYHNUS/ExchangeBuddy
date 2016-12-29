663dbb58bc6dc127429738c4dd91b571a82fd764
Irvin Lim <irvinlimweiquan@gmail.com>
2016-12-29 11:11:25 +0800

    Link GET /feedpostComment/:id

--- frontend/src/components/Group/GroupFeed/GroupFeedPost/GroupFeedPostComments/index.js
+++ frontend/src/components/Group/GroupFeed/GroupFeedPost/GroupFeedPostComments/index.js
@@ -1,155 +1,36 @@
-import { connect } from 'react-redux';
-import moment from 'moment';
-import ChildComponent from './GroupFeedPostComments';
+import React from 'react';
 
-const _tempCommentReplies = [
-  {
-    id: 1,
-    content: 'lmao yeah',
-    createdAt: moment().subtract(2, 'd').toDate(),
-    feedPostCommentId: 1,
-    author:     
-    {
-      id: 2,
-      name: 'Nancy Parker',
-      profilePictureUrl: 'http://lorempixel.com/80/80/people',
-      university: {
-        id: 2,
-        name: 'Fudan University',
-        city: 'Shanghai',
-        logoImageUrl: 'https://s3-ap-southeast-1.amazonaws.com/exchangebuddy-university-public-image/fudan-university.jpg',
-        country: {
-          alpha2Code: 'CN',
-          name: 'China',
-        },
-      },
-    },
-  },
+import { connect, PromiseState } from 'react-refetch';
+import { makeRefetch } from 'util/api';
+import { feedPostTransform } from 'util/propTypes';
 
-  {
-    id: 2,
-    content: 'hahahahahahaha',
-    createdAt: moment().subtract(2, 'd').toDate(),
-    feedPostCommentId: 1,
-    author: 
-    {
-      id: 3,
-      name: 'Margeret Pearson',
-      profilePictureUrl: 'http://lorempixel.com/80/80/people/2',
-      university: {
-        id: 3,
-        name: 'Stanford University',
-        city: 'Palo Alto',
-        logoImageUrl: 'https://s3-ap-southeast-1.amazonaws.com/exchangebuddy-university-public-image/stanford-university.jpg',
-        country: {
-          alpha2Code: 'US',
-          name: 'United States of America (USA)',
-        },
-      },
-    },
-  },
+import GroupFeedPostComments from './GroupFeedPostComments';
+import Loading from 'components/Loading';
+import ErrorComponent from 'components/Error';
 
-  {
-    id: 3, 
-    content: 'hello long time no see',
-    createdAt: moment().subtract(2, 'd').toDate(),
-    feedPostCommentId: 1,
-    author: 
-    { 
-      id: 1, 
-      name: 'Irvin Lim Wei Quan', 
-      fbUserId: '10155216317967575',
-      university: { 
-        id: 1, 
-        name: 'National University of Singapore', 
-        city: 'Singapore', 
-        logoImageUrl: 'https://s3-ap-southeast-1.amazonaws.com/exchangebuddy-university-public-image/national-university-of-singapore-nus.jpg',
-        country: {
-          alpha2Code: 'SG',
-          name: 'Singapore',
-        },
-      },
-    },
-  },
-];
+const Container = ({ commentsFetch, ...rest }) => {
+  if (commentsFetch.pending)
+    return <Loading />;
+  
+  if (commentsFetch.rejected)
+    return <ErrorComponent error={ commentsFetch.reason } />;
+  
+  return <GroupFeedPostComments feedComments={ commentsFetch.value } { ...rest } />;
+};
 
-const _tempFeedComments = [
-  {
-    id: 1,
-    createdAt: moment().subtract(2, 'd').toDate(),
-    feedPostId: 1,
-    content: 'Wow! Amazing insight! Thank you :)',
-    replies: _tempCommentReplies,
-    author:     
-    { 
-      id: 1, 
-      name: 'Irvin Lim Wei Quan', 
-      fbUserId: '10155216317967575',
-      university: { 
-        id: 1, 
-        name: 'National University of Singapore', 
-        city: 'Singapore', 
-        logoImageUrl: 'https://s3-ap-southeast-1.amazonaws.com/exchangebuddy-university-public-image/national-university-of-singapore-nus.jpg',
-        country: {
-          alpha2Code: 'SG',
-          name: 'Singapore',
-        },
-      },
-    },
-  },
+Container.propTypes = {
+  commentsFetch: React.PropTypes.instanceOf(PromiseState).isRequired,
+};
 
-  {
-    id: 2,
-    createdAt: moment().subtract(2, 'd').toDate(),
-    feedPostId: 1,
-    content: 'You\'re pretty awesome huh.',
-    replies: _tempCommentReplies,
-    author: 
-    {
-      id: 2,
-      name: 'Nancy Parker',
-      profilePictureUrl: 'http://lorempixel.com/80/80/people',
-      university: {
-        id: 2,
-        name: 'Fudan University',
-        city: 'Shanghai',
-        logoImageUrl: 'https://s3-ap-southeast-1.amazonaws.com/exchangebuddy-university-public-image/fudan-university.jpg',
-        country: {
-          alpha2Code: 'CN',
-          name: 'China',
-        },
-      },
-    },
-  },
+const refetch = ({ feedPost }) => {
+  const commentsFetch = makeRefetch(`/feedpostComment/${ feedPost.id }`, { userToken: true }, ({ data }) => data.map(feedPostTransform));
 
-  {
-    id: 3, 
-    createdAt: moment().subtract(2, 'd').toDate(),
-    feedPostId: 1,
-    content: 'What??? You serious?',
-    replies: _tempCommentReplies,
-    author: 
-    {
-      id: 3,
-      name: 'Margeret Pearson',
-      profilePictureUrl: 'http://lorempixel.com/80/80/people/2',
-      university: {
-        id: 3,
-        name: 'Stanford University',
-        city: 'Palo Alto',
-        logoImageUrl: 'https://s3-ap-southeast-1.amazonaws.com/exchangebuddy-university-public-image/stanford-university.jpg',
-        country: {
-          alpha2Code: 'US',
-          name: 'United States of America (USA)',
-        },
-      },
-    },
-  },
-];
+  return {
+    commentsFetch,
+    refreshComments: () => ({
+      commentsFetch: { ...commentsFetch, force: true, refreshing: true },
+    }),
+  };
+};
 
-const mapStateToProps = (state) => ({
-  user: state['User/currentUser'],
-  feedComments: _tempFeedComments,
-});
-
-export default connect(mapStateToProps)(ChildComponent);
+export default connect(refetch)(Container);
