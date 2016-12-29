579f043b02f459c2b678aff8af15e12e35a1d5bb
Irvin Lim <irvinlimweiquan@gmail.com>
2016-12-29 11:43:13 +0800

    Style GroupFeedPostWriteComment

--- frontend/src/components/Group/GroupFeed/GroupFeedPost/GroupFeedPostWriteComment/GroupFeedPostWriteComment.js
+++ frontend/src/components/Group/GroupFeed/GroupFeedPost/GroupFeedPostWriteComment/GroupFeedPostWriteComment.js
@@ -5,11 +5,13 @@ import AvatarRow from 'components/AvatarRow';
 import { TextFormField } from 'components/Field';
 import Icon from 'components/Icon';
 import IconButton from 'material-ui/IconButton';
+import Paper from 'components/Paper';
 
 import { getAvatarUrl } from 'util/user';
 import { userPropType, feedPostPropType } from 'util/propTypes';
 
 import * as Colors from 'material-ui/styles/colors';
+import './GroupFeedPostWriteComment.scss';
 
 const validate = ({ content }) => {
   const errors = {};
@@ -20,35 +22,37 @@ const validate = ({ content }) => {
   return errors;
 };
 
-const GroupWritePostForm = ({ feedPost, user, refreshComments, showSnackbar, submitPost, handleSubmit, reset }) => (
-  <form onSubmit={ handleSubmit(({ content }) => {
-    submitPost({ content, userId: user.id, feedPostId: feedPost.id }, () => {
-      reset();
-      showSnackbar('Comment added!');
-      refreshComments();
-    }, (err) => {
-      showSnackbar('Could not add comment: ' + err);
-    });
-  }) }>
-    <AvatarRow 
-      avatar={ getAvatarUrl(user) }
-      size={30} 
-      rightIcon={ 
-        <IconButton type="submit">
-          <Icon name="send" color={ Colors.grey500 } />
-        </IconButton> 
-      }>
-      <TextFormField 
-        name="content" 
-        hintText="Write a comment..." 
-        underlineShow={ false } 
-        style={{ fontSize: 14 }}
-        errorStyle={{ display: 'none' }} />
-    </AvatarRow>
-  </form>
+const GroupFeedPostWriteComment = ({ feedPost, user, refreshComments, showSnackbar, submitPost, handleSubmit, reset }) => (
+  <Paper full className="group-feed-post-write-comment">
+    <form onSubmit={ handleSubmit(({ content }) => {
+      submitPost({ content, userId: user.id, feedPostId: feedPost.id }, () => {
+        reset();
+        showSnackbar('Comment added!');
+        refreshComments();
+      }, (err) => {
+        showSnackbar('Could not add comment: ' + err);
+      });
+    }) }>
+      <AvatarRow 
+        avatar={ getAvatarUrl(user) }
+        size={30} 
+        rightIcon={ 
+          <IconButton type="submit">
+            <Icon name="send" color={ Colors.grey500 } />
+          </IconButton> 
+        }>
+        <TextFormField 
+          name="content" 
+          hintText="Write a comment..." 
+          underlineShow={ false } 
+          style={{ fontSize: 14 }}
+          errorStyle={{ display: 'none' }} />
+      </AvatarRow>
+    </form>
+  </Paper>
 );
 
-GroupWritePostForm.propTypes = {
+GroupFeedPostWriteComment.propTypes = {
   ...reduxFormPropTypes,
   user: userPropType.isRequired,
   feedPost: feedPostPropType.isRequired,
@@ -59,4 +63,4 @@ GroupWritePostForm.propTypes = {
 export default reduxForm({
   form: 'groupFeedPostWriteCommentForm',
   validate
-})(GroupWritePostForm);
+})(GroupFeedPostWriteComment);
