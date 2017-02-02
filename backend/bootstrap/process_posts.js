7c6c99ce665b97506e2ac9a266eb279a2ffaad12
WYHNUS <wangyanhao1993@163.com>
2017-02-02 21:34:29 +0800

    update uni bootstrap script

--- backend/bootstrap/process_posts.js
+++ backend/bootstrap/process_posts.js
@@ -3,15 +3,13 @@ var xml2js = require("xml2js");
 var app = require('../app');
 var models = require('../models');
 
-
 var parser = new xml2js.Parser();
 
 var dummy = {
     name: "ExchangeBuddy"
-}
+};
 
 models.User.create(dummy).then(function(user){
-
     fs.readFile('./exchangebuddy.wordpress.2016-11-03.xml', function(err, data){
         parser.parseString(data, function(err, result){
             for(var item of result.rss.channel[0].item){
@@ -33,5 +31,4 @@ models.User.create(dummy).then(function(user){
             }
         })
     })
-
-})
+});
---
+++ backend/bootstrap/uni_bootstrap.js
@@ -0,0 +1,29 @@
+var app = require('../app');
+var models = require('../models');
+var fs = require('fs');
+
+var removeDiacritics = require('diacritics').remove;
+var prefix = "https://s3-ap-southeast-1.amazonaws.com/exchangebuddy-university-public-image/";
+
+fs.readFile('./json/university_data.json', function(err, data) {
+    var universities = JSON.parse(data);
+
+    for (var i in universities) {
+        var uni = universities[i];
+
+        var logoName = uni.name.split('/')[0].trim().toLowerCase()
+            .replace(/ /g, '-')
+            .replace(')', '')
+            .replace('(', '');
+        logoName += '.jpg';
+        logoName = removeDiacritics(logoName);
+
+        models.University.create({
+            name: uni.name,
+            logoImageUrl: (fs.existsSync('./University_Logos/' + logoName)) ? (prefix + logoName) : null,
+            website: uni.website,
+            emailDomains: JSON.stringify(uni.emailDomains),
+            CountryAlpha2Code: uni.countryCode
+        });
+    }
+});
